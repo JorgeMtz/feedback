420442867c31b3454d57afb98b6b9209bd271ae1
Jorge Luis Martinez Mendoza <jorge@netcommerce.mx>
2022-06-27 16:47:31 -0500

    base added

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae reprehenderit tempora explicabo maiores eveniet neque sunt quam fugit minima sint! Porro odit, exercitationem quae itaque culpa nemo nostrum illo fugiat. Neque fuga doloribus rem eius temporibus, quae et enim animi nisi voluptatum natus velit vitae in quas suscipit blanditiis molestias mollitia aliquam illo magnam quam reiciendis sequi eveniet nihil. Exercitationem s!.',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+]
+
+export default FeedbackData;
