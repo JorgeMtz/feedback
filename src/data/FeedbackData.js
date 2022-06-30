f61bde1008ed6a777e5ebe544254110c12235485
JorgeMtz <geokg.j@gmail.com>
2022-06-29 23:17:01 -0500

    Update FeedbackData.js

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -2,12 +2,12 @@ const FeedbackData = [
     {
         id: 1,
         rating: 10,
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae reprehenderit tempora explicabo maiores eveniet neque sunt quam fugit minima sint! Porro odit, exercitationem quae itaque culpa nemo nostrum illo fugiat. Neque fuga doloribus rem eius temporibus, quae et enim animi nisi voluptatum natus velit vitae in quas suscipit blanditiis molestias mollitia aliquam illo magnam quam reiciendis sequi eveniet nihil. Exercitationem s!.',
+        text: 'Fer te amo mucho <3',
     },
     {
         id: 2,
         rating: 9,
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+        text: 'feedback sample text',
     },
     {
         id: 3,
